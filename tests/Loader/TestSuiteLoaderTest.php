52717713c92290b55c3bdb6f75f2b33697a8f040
Oliver Radwell <oliver.radwell@gmail.com>
2018-04-04 00:20:04 +0100

    TestSuiteLoaderTest is now an integration test for PHPUnit 7.0 compatibility

--- tests/Loader/TestSuiteLoaderTest.php
+++ tests/Loader/TestSuiteLoaderTest.php
@@ -4,6 +4,8 @@ namespace OckCyp\CoversValidator\Tests\Loader;
 
 use OckCyp\CoversValidator\Loader\TestSuiteLoader;
 use OckCyp\CoversValidator\Tests\BaseTestCase;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Util\Configuration;
 
 class TestSuiteLoaderTest extends BaseTestCase
 {
@@ -12,21 +14,11 @@ class TestSuiteLoaderTest extends BaseTestCase
      */
     public function testLoadsSuite()
     {
-        $testSuite = $this->getMockBuilder('PHPUnit\Framework\TestSuite')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $configuration = $this->getMockBuilder('PHPUnit\Util\Configuration')
-            ->disableOriginalConstructor()
-            ->setMethods(array('getTestSuiteConfiguration', 'handlePHPConfiguration'))
-            ->getMock();
-
-        $configuration->expects($this->once())
-            ->method('getTestSuiteConfiguration')
-            ->willReturn($testSuite);
+        $configuration = Configuration::getInstance('tests/Fixtures/configuration-existing.xml');
 
         $returnedSuite = TestSuiteLoader::loadSuite($configuration);
 
-        $this->assertEquals($testSuite, $returnedSuite);
+        $this->assertInstanceOf(TestSuite::class, $returnedSuite);
+        $this->assertEquals('My Test Suite', $returnedSuite->getName());
     }
 }
