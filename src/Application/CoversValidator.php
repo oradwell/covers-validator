1493114b7b3c4af442a7251ef12dc68abacb691e
Oliver Radwell <oliver.radwell@gmail.com>
2020-09-26 11:07:12 +0100

    Bump app version to v1.3.0

--- src/Application/CoversValidator.php
+++ src/Application/CoversValidator.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 class CoversValidator extends Application
 {
     const NAME = 'CoversValidator';
-    const VERSION = '1.2.0';
+    const VERSION = '1.3.0';
 
     /**
      * {@inheritdoc}
