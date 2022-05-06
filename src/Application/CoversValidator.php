ed4564880c71f81ad6e877e975453a247436a73d
Damien Debin <ddebin@users.noreply.github.com>
2022-05-06 13:22:14 +0200

    Allow `symfony/console` 6.*

--- src/Application/CoversValidator.php
+++ src/Application/CoversValidator.php
@@ -2,50 +2,98 @@
 
 namespace OckCyp\CoversValidator\Application;
 
+use Composer\InstalledVersions;
 use OckCyp\CoversValidator\Command\ValidateCommand;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 
-class CoversValidator extends Application
-{
-    const NAME = 'CoversValidator';
-    const VERSION = '1.4.0';
-
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct($name = self::NAME, $version = self::VERSION)
+if (version_compare(InstalledVersions::getVersion('symfony/console'), '6.0.0') >= 0) {
+    class CoversValidator extends Application
     {
-        parent::__construct($name, $version);
+        const NAME = 'CoversValidator';
+        const VERSION = '1.4.0';
+
+        /**
+         * {@inheritdoc}
+         */
+        public function __construct($name = self::NAME, $version = self::VERSION)
+        {
+            parent::__construct($name, $version);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        protected function getCommandName(InputInterface $input): ?string
+        {
+            return 'validate';
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        protected function getDefaultCommands(): array
+        {
+            $defaultCommands = parent::getDefaultCommands();
+            $defaultCommands[] = new ValidateCommand;
+
+            return $defaultCommands;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getDefinition(): InputDefinition
+        {
+            $inputDefinition = parent::getDefinition();
+            $inputDefinition->setArguments();
+
+            return $inputDefinition;
+        }
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getCommandName(InputInterface $input)
+} else {
+    class CoversValidator extends Application
     {
-        return 'validate';
-    }
+        const NAME = 'CoversValidator';
+        const VERSION = '1.4.0';
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getDefaultCommands()
-    {
-        $defaultCommands = parent::getDefaultCommands();
-        $defaultCommands[] = new ValidateCommand;
+        /**
+         * {@inheritdoc}
+         */
+        public function __construct($name = self::NAME, $version = self::VERSION)
+        {
+            parent::__construct($name, $version);
+        }
 
-        return $defaultCommands;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        protected function getCommandName(InputInterface $input)
+        {
+            return 'validate';
+        }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getDefinition()
-    {
-        $inputDefinition = parent::getDefinition();
-        $inputDefinition->setArguments();
+        /**
+         * {@inheritdoc}
+         */
+        protected function getDefaultCommands()
+        {
+            $defaultCommands = parent::getDefaultCommands();
+            $defaultCommands[] = new ValidateCommand;
 
-        return $inputDefinition;
+            return $defaultCommands;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getDefinition()
+        {
+            $inputDefinition = parent::getDefinition();
+            $inputDefinition->setArguments();
+
+            return $inputDefinition;
+        }
     }
 }
