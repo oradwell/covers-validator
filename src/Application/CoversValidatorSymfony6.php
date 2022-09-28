4c9507b1e297099416eb7bef355320c4acb72dca
Théo FIDRY <theo.fidry@gmail.com>
2022-09-28 10:04:12 +0200

    Add Symfony6 compatibility

---
+++ src/Application/CoversValidatorSymfony6.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace OckCyp\CoversValidator\Application;
+
+use Composer\InstalledVersions;
+use OckCyp\CoversValidator\Command\ValidateCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+
+class CoversValidatorSymfony6 extends Application
+{
+    const NAME = 'CoversValidator';
+    const VERSION = '1.4.0';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($name = self::NAME, $version = self::VERSION)
+    {
+        parent::__construct($name, $version);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCommandName(InputInterface $input): ?string
+    {
+        return 'validate';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultCommands(): array
+    {
+        $defaultCommands = parent::getDefaultCommands();
+        $defaultCommands[] = new ValidateCommand;
+
+        return $defaultCommands;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition(): InputDefinition
+    {
+        $inputDefinition = parent::getDefinition();
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
+}
