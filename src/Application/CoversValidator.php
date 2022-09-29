1a52dc0f02414907ec943892b64f90f707636af1
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2022-09-30 00:18:29 +0200

    Add Symfony6 compatibility (#41)
    Co-authored-by: Damien Debin <ddebin@users.noreply.github.com>
    Co-authored-by: Oliver Radwell <oliver.radwell@gmail.com>

--- src/Application/CoversValidator.php
+++ src/Application/CoversValidator.php
@@ -2,50 +2,16 @@
 
 namespace OckCyp\CoversValidator\Application;
 
-use OckCyp\CoversValidator\Command\ValidateCommand;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
+use Composer\InstalledVersions;
+use function class_alias;
+use function version_compare;
 
-class CoversValidator extends Application
-{
-    const NAME = 'CoversValidator';
-    const VERSION = '1.4.0';
+$isSymfony6 = version_compare(
+    InstalledVersions::getVersion('symfony/console'),
+    '6.0.0'
+) >= 0;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct($name = self::NAME, $version = self::VERSION)
-    {
-        parent::__construct($name, $version);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getCommandName(InputInterface $input)
-    {
-        return 'validate';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getDefaultCommands()
-    {
-        $defaultCommands = parent::getDefaultCommands();
-        $defaultCommands[] = new ValidateCommand;
-
-        return $defaultCommands;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDefinition()
-    {
-        $inputDefinition = parent::getDefinition();
-        $inputDefinition->setArguments();
-
-        return $inputDefinition;
-    }
-}
+class_alias(
+    $isSymfony6 ? CoversValidatorSymfony6::class : CoversValidatorSymfony5::class,
+    'OckCyp\CoversValidator\Application\CoversValidator'
+);
